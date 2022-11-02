ddfec2410a83c8e76a26a4e722852bb576adab65
fynefav3 <agbaruihuoma@gmail.com>
2022-11-02 23:39:36 +0100

    Made a correction on tool file Bug:T318921

--- src/component/Record/Tool.js
+++ src/component/Record/Tool.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
